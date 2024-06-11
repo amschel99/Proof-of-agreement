d0fcaf0a99d3cd4c1d198a7a16c23c4caffb4142
amschel99 <6290.2020@students.ku.ac.ke>
2024-06-11 11:17:19 +0300

    fixes

--- src/pok_backend/src/lib.rs
+++ src/pok_backend/src/lib.rs
@@ -82,40 +82,8 @@ fn _agree_to_agreement(user: String, agreement: Agreement) -> Agreement {
     let agreeing_party = Principal::principal_to_user(user);
     agreeing_party.agree(agreement)
 }
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn agreement_btwn_god_and_man() {
-        let terms: Vec<String> = vec![
-        "I am the Lord thy God".to_string(),
-        "Thou shalt have no other gods before me".to_string(),
-        "Thou shalt not make unto thee any graven image".to_string(),
-        "Thou shalt not take the name of the Lord thy God in vain".to_string(),
-        "Remember the sabbath day, to keep it holy".to_string(),
-        "Honour thy father and thy mother".to_string(),
-        "Thou shalt not kill".to_string(),
-        "Thou shalt not commit adultery".to_string(),
-        "Thou shalt not steal".to_string(),
-        "Thou shalt not bear false witness against thy neighbour".to_string(),
-        "Thou shalt not covet thy neighbour's house".to_string(),
-        "Thou shalt not covet thy neighbour's wife, nor his manservant, nor his maidservant, nor his ox, nor his ass, nor any thing that is thy neighbour's".to_string(),
-    ];
-        let agreement =
-            _create_new_agreement(terms, String::from("God"), 1, String::from("the heck"));
-        let amschel_agrees = _agree_to_agreement(String::from("God"), agreement);
-        dbg!(amschel_agrees.proof_of_agreement.unwrap().0.unwrap().value);
-    }
-    #[test]
-    fn _agree_to_agreement_works() {}
-}
 
 // Internet computer functions here
-
-#[ic_cdk::query]
-fn check_status() -> String {
-    String::from("We are live")
-}
 
 #[ic_cdk::update]
 
